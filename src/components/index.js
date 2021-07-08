99fdeabcc6baebf06f15b54c54c711ee4724213a
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-08 17:40:05 +0500

    added TriStateButton component to UI

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Todo from "./Todo";
 import EmptyList from './EmptyList'
 import TodoForm from './TodoForm'
+import TriStateButton from "./TriStateButton";
 
 export {
     Todo,
     EmptyList,
-    TodoForm
+    TodoForm,
+    TriStateButton
 }
