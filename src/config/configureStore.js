b54ec325b8d6014d148ba4e2ecaab1e052e8dd9d
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-08 18:02:51 +0500

    filterSlice combined in rootReducer

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,5 +1,5 @@
-import rootReducer from '../reducers'
-import {configureStore} from '@reduxjs/toolkit'
+import {textSlice, todoSlice, filterSlice} from '../reducers'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
@@ -8,18 +8,36 @@ const initialState = {
         {id: "12345", todo: "Task 2", isCompleted: false},
         {id: "23456", todo: "Task 3", isCompleted: false},
         {id: "45678", todo: "Task 4", isCompleted: false}
-    ]
+    ],
+    text: 'test',
+    filters: {
+        status : "all"
+    }
 }
 
 const persistConfig = {
-key: 'root',
-storage
+    key: 'root',
+    storage,
+    blacklist: ['text']
 }
 
+const alwaysReturnHelloMiddleware = storeAPI => next => action => {
+    next(action);
+    // Ignore the original result, return something else
+}
+
+const rootReducer = combineReducers({
+    todos: todoSlice,
+    text: textSlice,
+    filters: filterSlice
+})
+
+// const middlewareEnhancer = applyMiddleware(alwaysReturnHelloMiddleware)
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: [alwaysReturnHelloMiddleware],
   preloadedState: initialState
 
 })
