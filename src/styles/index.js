01ee963715dd59140096b5d59a0a25f7cfb03798
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-28 14:11:31 +0500

    added signup screen

---
+++ src/styles/index.js
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+    authContainer: {
+      marginTop: "8%"
+    },
+    authPaperWrapper: {
+      padding: theme.spacing(4),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    authPaper: {
+      marginTop: theme.spacing(3),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: "#558b2f",
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+      background: "#558b2f"
+    },
+  }));
+  export default useStyles
---
+++ src/views/SignUp/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Avatar, Button, Container, CssBaseline, 
+  Grid, Link, Paper, TextField, Typography } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import useStyles from '../../styles'
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.authContainer} xs={12} component="main" maxWidth="sm">
+      <Paper className={classes.authPaperWrapper}>
+      <CssBaseline />
+      <div className={classes.authPaper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      </Paper>
+      </Container>
+  );
+}
